897c254da4193ee8d269f3cd07f542604638db36
Alfur 'Felt her' Deltur <admin@skid.pro>
2015-06-30 11:42:52 +1200

    Settings Work
    Added keybind logic to Settings form

--- MidoriDesktop/Settings.Designer.cs
+++ MidoriDesktop/Settings.Designer.cs
@@ -28,24 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabs = new System.Windows.Forms.TabControl();
             this.tabGeneral = new System.Windows.Forms.TabPage();
             this.tabHotkeys = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHotkeyImage = new System.Windows.Forms.Button();
+            this.txtHotkeyImage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHotkeyVideo = new System.Windows.Forms.Button();
+            this.txtHotkeyVideo = new System.Windows.Forms.TextBox();
+            this.tabs.SuspendLayout();
+            this.tabHotkeys.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tabs
             // 
-            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tabs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl1.Controls.Add(this.tabGeneral);
-            this.tabControl1.Controls.Add(this.tabHotkeys);
-            this.tabControl1.Location = new System.Drawing.Point(-1, -1);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(441, 230);
-            this.tabControl1.TabIndex = 0;
+            this.tabs.Controls.Add(this.tabGeneral);
+            this.tabs.Controls.Add(this.tabHotkeys);
+            this.tabs.Location = new System.Drawing.Point(-1, -1);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(441, 230);
+            this.tabs.TabIndex = 0;
             // 
             // tabGeneral
             // 
@@ -59,6 +66,12 @@
             // 
             // tabHotkeys
             // 
+            this.tabHotkeys.Controls.Add(this.txtHotkeyVideo);
+            this.tabHotkeys.Controls.Add(this.txtHotkeyImage);
+            this.tabHotkeys.Controls.Add(this.btnHotkeyVideo);
+            this.tabHotkeys.Controls.Add(this.label2);
+            this.tabHotkeys.Controls.Add(this.btnHotkeyImage);
+            this.tabHotkeys.Controls.Add(this.label1);
             this.tabHotkeys.Location = new System.Drawing.Point(4, 22);
             this.tabHotkeys.Name = "tabHotkeys";
             this.tabHotkeys.Padding = new System.Windows.Forms.Padding(3);
@@ -67,24 +80,94 @@
             this.tabHotkeys.Text = "Hotkeys";
             this.tabHotkeys.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Capture image:";
+            // 
+            // btnHotkeyImage
+            // 
+            this.btnHotkeyImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHotkeyImage.Location = new System.Drawing.Point(397, 6);
+            this.btnHotkeyImage.Name = "btnHotkeyImage";
+            this.btnHotkeyImage.Size = new System.Drawing.Size(25, 23);
+            this.btnHotkeyImage.TabIndex = 1;
+            this.btnHotkeyImage.Text = "...";
+            this.btnHotkeyImage.UseVisualStyleBackColor = true;
+            this.btnHotkeyImage.Click += new System.EventHandler(this.btnHotkeyImage_Click);
+            // 
+            // txtHotkeyImage
+            // 
+            this.txtHotkeyImage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHotkeyImage.BackColor = System.Drawing.SystemColors.Window;
+            this.txtHotkeyImage.Location = new System.Drawing.Point(93, 8);
+            this.txtHotkeyImage.Name = "txtHotkeyImage";
+            this.txtHotkeyImage.ReadOnly = true;
+            this.txtHotkeyImage.Size = new System.Drawing.Size(298, 20);
+            this.txtHotkeyImage.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Capture video:";
+            // 
+            // btnHotkeyVideo
+            // 
+            this.btnHotkeyVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHotkeyVideo.Location = new System.Drawing.Point(397, 35);
+            this.btnHotkeyVideo.Name = "btnHotkeyVideo";
+            this.btnHotkeyVideo.Size = new System.Drawing.Size(25, 23);
+            this.btnHotkeyVideo.TabIndex = 1;
+            this.btnHotkeyVideo.Text = "...";
+            this.btnHotkeyVideo.UseVisualStyleBackColor = true;
+            this.btnHotkeyVideo.Click += new System.EventHandler(this.btnHotkeyVideo_Click);
+            // 
+            // txtHotkeyVideo
+            // 
+            this.txtHotkeyVideo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHotkeyVideo.BackColor = System.Drawing.SystemColors.Window;
+            this.txtHotkeyVideo.Location = new System.Drawing.Point(93, 37);
+            this.txtHotkeyVideo.Name = "txtHotkeyVideo";
+            this.txtHotkeyVideo.ReadOnly = true;
+            this.txtHotkeyVideo.Size = new System.Drawing.Size(298, 20);
+            this.txtHotkeyVideo.TabIndex = 2;
+            // 
             // Settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(437, 227);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tabs);
             this.Name = "Settings";
             this.ShowIcon = false;
             this.Text = "Midori Settings";
-            this.tabControl1.ResumeLayout(false);
+            this.tabs.ResumeLayout(false);
+            this.tabHotkeys.ResumeLayout(false);
+            this.tabHotkeys.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tabs;
         private System.Windows.Forms.TabPage tabGeneral;
         private System.Windows.Forms.TabPage tabHotkeys;
+        private System.Windows.Forms.TextBox txtHotkeyImage;
+        private System.Windows.Forms.Button btnHotkeyImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHotkeyVideo;
+        private System.Windows.Forms.Button btnHotkeyVideo;
+        private System.Windows.Forms.Label label2;
     }
 }
--- MidoriDesktop/Settings.cs
+++ MidoriDesktop/Settings.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Gma.UserActivityMonitor;
 
 namespace MidoriDesktop
 {
@@ -13,6 +14,61 @@ namespace MidoriDesktop
         public Settings()
         {
             InitializeComponent();
+        }
+
+        bool ctrl = false, alt = false, shift = false;
+        void KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Control || e.KeyCode == Keys.ControlKey || e.KeyCode == Keys.LControlKey || e.KeyCode == Keys.RControlKey) ctrl = true;
+            else if (e.KeyCode == Keys.Alt) alt = true;
+            else if (e.KeyCode == Keys.Shift || e.KeyCode == Keys.ShiftKey || e.KeyCode == Keys.LShiftKey || e.KeyCode == Keys.RShiftKey) shift = true;
+            else if (imagefocus) txtHotkeyImage.Text = (ctrl ? "Ctrl + " : "") + (alt ? "Alt + " : "") + (shift ? "Shift + " : "") + e.KeyCode.ToString();
+            else if (videofocus) txtHotkeyVideo.Text = (ctrl ? "Ctrl + " : "") + (alt ? "Alt + " : "") + (shift ? "Shift + " : "") + e.KeyCode.ToString();
+        }
+
+        void KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Control || e.KeyCode == Keys.ControlKey || e.KeyCode == Keys.LControlKey || e.KeyCode == Keys.RControlKey) ctrl = false;
+            else if (e.KeyCode == Keys.Alt) alt = false;
+            else if (e.KeyCode == Keys.Shift || e.KeyCode == Keys.ShiftKey || e.KeyCode == Keys.LShiftKey || e.KeyCode == Keys.RShiftKey) shift = false;
+        }
+
+        void HookKeyboard(bool flag)
+        {
+            if (flag)
+            {
+                HookManager.KeyDown += KeyDown;
+                HookManager.KeyUp += KeyUp;
+
+                this.ControlBox = false;
+            }
+            else
+            {
+                HookManager.KeyDown -= KeyDown;
+                HookManager.KeyUp -= KeyUp;
+
+                this.ControlBox = true;
+            }
+        }
+
+        bool imagefocus = false;
+        private void btnHotkeyImage_Click(object sender, EventArgs e)
+        {
+            btnHotkeyVideo.Enabled = imagefocus;
+
+            imagefocus = !imagefocus;
+
+            HookKeyboard(imagefocus);
+        }
+
+        bool videofocus = false;
+        private void btnHotkeyVideo_Click(object sender, EventArgs e)
+        {
+            btnHotkeyImage.Enabled = videofocus;
+
+            videofocus = !videofocus;
+
+            HookKeyboard(videofocus);
         }
     }
 }
